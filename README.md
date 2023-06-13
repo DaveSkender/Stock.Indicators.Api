d1d9505fc658c532f8285ca43fe34c17fc3c0fba
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-06-12 23:54:03 -0400

    boilerplate projects

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Stock.Indicators.Api
-A self-hosted REST API that returns stock indicators, based on our .NET library.
+# Stock Indicators API
+
+A self-hosted REST API that returns stock indicators, based on our [.NET library](https://dotnet.stockindicators.dev).
---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+// STARTUP CONFIGURATION
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+IServiceCollection services = builder.Services;
+ConfigurationManager configuration = builder.Configuration;
+
+// add framework services
+services.AddControllers();
+
+// setup CORS for website
+IConfigurationSection corsOrigins = configuration.GetSection("CorsOrigins");
+
+services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+    cors =>
+    {
+        cors.AllowAnyHeader();
+        cors.AllowAnyMethod();
+        //cors.WithOrigins(corsOrigins["Website"]);
+    });
+});
+
+// build application
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+_ = app.Environment.IsDevelopment()
+  ? app.UseDeveloperExceptionPage()
+  : app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+app.MapControllers();
+app.Run();
